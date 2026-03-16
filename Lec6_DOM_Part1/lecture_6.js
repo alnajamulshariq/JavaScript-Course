1e78e4d4b69ece14e87f314ff71e01f79a7f6079
ShariqNajam <mschariquen@gmail.com>
2026-03-17 02:25:38 +0500

    JavaScript DOM - Document Object Model | Part-1

--- Lec6_DOM_Part1/lecture_6.js
+++ Lec6_DOM_Part1/lecture_6.js
@@ -15,10 +15,7 @@
 // ii- document.querySelectorAll("myId / myClass / tag");
 //    its returns a NodeList
 
-
-
-
-// Let's Try to get Elements by above methods 
+// Let's Try to get Elements by above methods
 
 // 1- Selecting with ID:
 // document.getElementById("myId");
@@ -26,20 +23,17 @@ let titleName = document.getElementById("nameTitle");
 console.dir(titleName);
 console.log(titleName);
 
-
 // 2- Selecting with CLASS:
 // document.getElementsByClassName("myClass");
 let headings = document.getElementsByClassName("jsDom");
 console.dir(headings);
 console.log(headings);
 
-
 // 3- Selecting with TAG:
 // document.getElementsByTagName("p");
 let getTags = document.getElementsByTagName("p");
 console.dir(getTags);
 console.log(getTags);
-
 
 // 4- Query Selector:
 //  i- document.querySelector("myId / myClass / tag");
@@ -49,7 +43,6 @@ let firstEl = document.querySelector("p");
 console.dir(firstEl);
 console.log(firstEl);
 
-
 // 4- Query Selector:
 // ii- document.querySelectorAll("myId / myClass / tag");
 //    its returns a NodeList
@@ -58,7 +51,6 @@ let allEl = document.querySelectorAll("p");
 console.dir(allEl);
 console.log(allEl);
 
-
 let btn = document.querySelector("#btn");
 console.dir(btn);
 console.log(btn);
@@ -66,11 +58,10 @@ console.log(btn);
 let parent = document.querySelector(".outer");
 let allChildren = parent.children;
 for (let i = 0; i < allChildren.length; i++) {
-    allChildren[i].style.color = "blue";
+  allChildren[i].style.color = "blue";
 }
 parent.firstElementChild.style.backgroundColor = "red";
 parent.lastElementChild.style.backgroundColor = "yellow";
-
 
 // DOM Manipulation Properties:
 //  i- tagName: returns tag for element nodes
@@ -80,14 +71,12 @@ parent.lastElementChild.style.backgroundColor = "yellow";
 
 let div = document.querySelector(".div");
 
-
-// Let's Practice 
+// Let's Practice
 // Q-1: Create a h2 heading element with text "Hello JavaScript". Append "from Apna College Students" to
 //      this text using JS.
 
 let h2 = document.querySelector(".h2");
 h2.innerText = h2.innerText + " from Apna College Students";
-
 
 // Q-2:Create 3 divs with common class name -"box". Access them & add some unique text to each of them
 
@@ -96,14 +85,14 @@ h2.innerText = h2.innerText + " from Apna College Students";
 // for (const div of divBox) {
 //     div.innerText = `this is our ${index} unique div box`
 //     index++;
+//
 // }
 
 let divBox = document.querySelectorAll(".box");
 for (let index = 0; index <= 3; index++) {
-    divBox[index].innerText = `This is our ${index + 1} unique div box`;
+  divBox[index].innerText = `This is our ${index + 1} unique div box`;
 }
 
 for (let index = 1; index <= 10; index++) {
-    document.write(index + "<br>");
-
+  document.write(index + "<br>");
 }
